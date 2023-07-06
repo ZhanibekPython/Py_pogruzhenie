6dac58f929b434e38bbf5c475b2348d132a1bced
Zhanibek <astic1@mail.ru>
2023-07-07 01:29:37 +0600

    Sem#6_HW_done

---
+++ Sem#6_HW/Sem#6_package/Sem#6_HW_Ferzi_task#2.py
@@ -0,0 +1,35 @@
+from sys import exit
+
+N = 8
+
+matrix = [[0] * N for i in range(N)]
+for i in range(N):
+    x, v = map(int, input("Введите координаты положения восьми ферзей: ").split())
+    matrix[x-1][v-1] = 1
+
+flag = True
+
+def stop_iter():
+    global flag
+    flag = False
+    print(flag)
+    sys.exit()
+
+for i in range(N):
+    count_i = 0
+    count_j = 0
+    for j in range(N):
+        if matrix[i].count(1) >= 2:
+            stop_iter()
+        elif matrix[j].count(1) >= 2:
+            stop_iter()
+        elif matrix[i][j+1] == 1:
+            count_i += 1
+            if count_i > 1:
+                stop_iter()
+        elif matrix[j][x+1] == 1:
+            count_j += 1
+            if count_j > 1:
+                stop_iter()
+
+print(flag)
