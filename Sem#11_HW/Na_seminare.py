e174a7febf8f3e48b19eabbb105005cda4d03a7d
Zhanibek <astic1@mail.ru>
2023-07-28 22:06:47 +0600

    Sem#12_HW_done

--- Sem#11_HW/Na_seminare.py
+++ Sem#11_HW/Na_seminare.py
@@ -1,30 +1,32 @@
+import time
 
-# import time
-#
-# class My_string(str):
-#
-#     def __new__(cls, author: str, value):
-#         instance = super().__new__(cls, value)
-#         instance.author = author
-#         instance.time = time.time()
-#         return instance
-#
-#     def __str__(self):
-#         return f'Для пользователя {self.author}, {self.time}'
-#
-#     def __repr__(self):
-#         return f'Для программиста {self.author}, {self.time}'
-#
-#
-#
-#
-# example1 = My_string('Name', 'Bob')
-# print(example1)
-# print(repr(example1))
+
+class My_string(str):
+    """The class is inherited from str,
+    returns name and time of printing for
+    both a user and a developer """
+
+    def __new__(cls, author: str, value):
+        instance = super().__new__(cls, value)
+        instance.author = author
+        instance.time = time.time()
+        return instance
+
+    def __str__(self):
+        return f'Для пользователя {self.author}, {self.time}'
+
+    def __repr__(self):
+        return f'Для программиста {self.author}, {self.time}'
+
+
+example1 = My_string('Name', 'Bob')
+print(example1)
+print(repr(example1))
 
 
 class Rectangle:
-
+    """This class compares the values of two rectangles and also
+    returns addition, substraction, division and multiplication results"""
     def __init__(self, width: float, length: float):
         self.width = width
         self.length = length
@@ -46,7 +48,7 @@ class Rectangle:
 
     def __truediv__(self, other):
         return self.sq_area() / other.sq_area() if (self.sq_area() > other.sq_area()) else (
-                    other.sq_area() / self.sq_area())
+                other.sq_area() / self.sq_area())
 
     def __eq__(self, other):
         return self.sq_area() == other.sq_area()
@@ -66,8 +68,8 @@ class Rectangle:
     def __le__(self, other):
         return self.sq_area() <= other.sq_area()
 
+
 a = Rectangle(3, 4)
 b = Rectangle(5, 6)
-
 
 print(a + b, a - b, a * b, a / b, a == b, a != b, a < b, a > b, a >= b, a <= b, sep='\n')
